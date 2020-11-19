39eea3add582777ebc5a6f22250feb27638a5793
Ashish M J <ashm.jagadeesh@gmail.com>
2020-11-19 13:57:31 +0530

    Update file

--- app.py
+++ app.py
@@ -5,4 +5,35 @@ Created on Thu Nov 19 13:20:57 2020
 
 @author: ashish
 """
+from flask import Flask, render_template
+from newsapi import NewsApiClient
 
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    newsapi = NewsApiClient(api_key='dcbd16c218ef4985a822b478b4f20f49')
+    top_headlines = newsapi.get_top_headlines(sources='cricbuzz,cricket-news',
+                                          category='sports',
+                                          language='en',
+                                          country='in')
+    
+    articles = top_headlines['articles']
+    
+    desc,news,img = [],[],[]
+    
+    for i in range(len(articles)):
+        my_articles = articles[i]
+        
+        news.append(my_articles['titles'])
+        desc.append(my_articles['description'])
+        img.append(my_articles['urltoImage'])
+        
+    my_list = zip(desc, news, img)
+    return render_template('home.html',my_list=my_list)
+
+if __name__=="__main__": 
+    app.run(debug='False')
+        
+    
